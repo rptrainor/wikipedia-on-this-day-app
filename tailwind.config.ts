edaf972efcaa790f11dbce965bddda82cd2b5269
Ryan Trainor <rptrainor@gmail.com>
2024-06-11 22:58:09 -0300

    add brand colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,19 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-      },
+      colors: {
+        "brand_primary": "#5C53DC",
+        "brand_primary_light": "#B4AEEB",
+        "brand_primary_dark": "#4D49C3",
+        "brand_secondary": "#B61BA9",
+        "brand_secondary_light": "#F15FE1",
+        "brand_secondary_dark": "#7D0074",
+        "brand_gray": "#E5E5EA",
+        "brand_gray_light": "#A9A9A9",
+        "brand_gray_dark": "#7A7A7B",
+        "brand_background": "#F5F5F5",
+        "brand_prose": "#333333",
+      }
     },
   },
   plugins: [],
