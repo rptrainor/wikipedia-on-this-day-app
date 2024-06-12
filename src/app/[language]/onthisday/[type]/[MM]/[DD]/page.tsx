195306f1fd3891e1c27782420d8319088e3d4961
Ryan Trainor <rptrainor@gmail.com>
2024-06-12 10:18:00 -0300

    remove react query

--- src/app/[language]/onthisday/[type]/[MM]/[DD]/page.tsx
+++ src/app/[language]/onthisday/[type]/[MM]/[DD]/page.tsx
@@ -1,32 +1,19 @@
 "use client";
 
 import React from "react";
-import { QueryClient, QueryClientProvider, type QueryClientConfig } from '@tanstack/react-query';
 import { type OnThisDayPageProps } from "~/features/wikipedia/births/types";
 import { getDateInfo } from "~/features/wikipedia/utilities";
-import { BirthsList } from "~/features/wikipedia/births/components";
-
-const queryClientConfig: QueryClientConfig = {
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-};
-
-const queryClient = new QueryClient(queryClientConfig);
+// import { BirthsList } from "~/features/wikipedia/births/components";
 
 const OnThisDayPage: React.FC<OnThisDayPageProps> = ({ params }) => {
   const { month, dayWithSuffix } = getDateInfo(parseInt(params.DD), parseInt(params.MM));
   return (
-    <QueryClientProvider client={queryClient}>
       <main className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <div>
           <h1>Who was born on the {dayWithSuffix} of {month}?</h1>
-          <BirthsList language={params.language} type={params.type} MM={params.MM} DD={params.DD} />
+          {/* <BirthsList language={params.language} type={params.type} MM={params.MM} DD={params.DD} /> */}
         </div>
       </main>
-    </QueryClientProvider>
   )
 };
 
--- src/features/wikipedia/births/components.tsx
+++ src/features/wikipedia/births/components.tsx
@@ -1,39 +1,38 @@
 import React from 'react';
-import { useQuery } from '@tanstack/react-query';
 
-import { type GetTodayInHistoryParams, type WikipediaApiBirthTypeResponse } from './types';
+// import { type GetTodayInHistoryParams, type WikipediaApiBirthTypeResponse } from './types';
 
-export const BirthsList = (props: GetTodayInHistoryParams) => {
-  const { language, type, MM, DD } = props;
+export const BirthsList = () => {
+  // const { language, type, MM, DD } = props;
 
-  const { data, error, isLoading } = useQuery<WikipediaApiBirthTypeResponse, Error>({
-    queryKey: ['births', language, type, MM, DD],
-    queryFn: async () => {
-      const response = await fetch(`/api/onthisday?language=${language}&type=${type}&MM=${MM}&DD=${DD}`);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    },
-  });
+  // const { data, error, isLoading } = useQuery<WikipediaApiBirthTypeResponse, Error>({
+  //   queryKey: ['births', language, type, MM, DD],
+  //   queryFn: async () => {
+  //     const response = await fetch(`/api/onthisday?language=${language}&type=${type}&MM=${MM}&DD=${DD}`);
+  //     if (!response.ok) {
+  //       throw new Error('Network response was not ok');
+  //     }
+  //     return response.json();
+  //   },
+  // });
 
-  if (error && !isLoading) {
-    return <ErrorModal error={error.message} />;
-  }
+  // if (error && !isLoading) {
+  //   return <ErrorModal error={error.message} />;
+  // }
 
-  if (isLoading) {
-    return <Loading />;
-  }
+  // if (isLoading) {
+  //   return <Loading />;
+  // }
 
   return (
     <ul>
-      {data?.births.map((birth: { text: string; pages: { title: string }[] }) => (
+      {/* {data?.births.map((birth: { text: string; pages: { title: string }[] }) => (
         <li key={birth.text}>
           <a href={`/en/onthisday/birth/${birth.pages[0]?.title}`}>
             {birth.text}
           </a>
         </li>
-      ))}
+      ))} */}
     </ul>
   );
 };
