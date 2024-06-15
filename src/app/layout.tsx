8e6b3b59b96ee70c35bc3e1861cb1a3e9301e90f
Ryan Trainor <rptrainor@gmail.com>
2024-06-15 14:34:17 -0300

    update typogrpahy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen bg-background font-sans antialiased font-semibold text-lg",
           fontSans.variable
         )}
       >
