96abd0d190417d6ba267e71e0f141d06391f66c7
Ryan Trainor <rptrainor@gmail.com>
2024-06-15 14:03:22 -0300

    add theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,87 @@
-import { type Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-export default {
-  content: ["./src/**/*.tsx"],
+const config = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        "brand_primary": "#5C53DC",
-        "brand_primary_light": "#B4AEEB",
-        "brand_primary_dark": "#4D49C3",
-        "brand_secondary": "#B61BA9",
-        "brand_secondary_light": "#F15FE1",
-        "brand_secondary_dark": "#7D0074",
-        "brand_gray": "#E5E5EA",
-        "brand_gray_light": "#A9A9A9",
-        "brand_gray_dark": "#7A7A7B",
-        "brand_background": "#F5F5F5",
-        "brand_prose": "#333333",
+        'brand_primary': "hsl(243, 66, 59)",
+        'brand_primary_light': "hsl(243, 66, 59)",
+        'brand_primary_dark': "hsl(241, 50, 52)",
+        'brand_secondary': "hsl(305, 74, 40)",
+        'brand_secondary_light': "hsl(306, 83, 65)",
+        'brand_secondary_dark': "hsl(304, 100, 24)",
+        'brand_gray': "hsl(240, 10, 90)",
+        'brand_gray_light': "hsl(0, 0, 66)",
+        'brand_gray_dark': "hsl(240, 0, 48)",
+        // 'brand_background': "hsl(0, 0, 96)",
+        'brand_prose': "hsl(0, 0, 20)",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       screens: {
         "xs": "320px",
@@ -26,5 +92,7 @@ export default {
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
