70b09afa945c764e9b784acbec4d256426e74951
Ryan Trainor <rptrainor@gmail.com>
2024-06-15 21:35:02 -0300

    adjust date picker styles

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,7 +7,7 @@ import {
 export function Modal({ children, isOpen, onClose }: { children: React.ReactNode, isOpen: boolean, onClose: () => void }) {
   return (
     <Dialog open={isOpen} onOpenChange={onClose} modal>
-      <DialogContent className="sm:max-w-md xs:max-w-[calc(100vw-2rem)] w-full h-auto mx-auto rounded-md xs:p-2 xs:pt-10">
+      <DialogContent className="sm:max-w-md xs:max-w-[calc(100vw-2rem)] w-auto h-auto mx-auto rounded-md xs:p-2 xs:pt-10">
         {children}
       </DialogContent>
     </Dialog>
